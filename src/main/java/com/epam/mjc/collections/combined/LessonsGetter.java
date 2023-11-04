b955ce633132262034656a7aaffa7f01792c1406
boris <baizenbe@gmail.com>
2023-11-04 18:46:24 +0600

    collected

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,25 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> result = new HashSet<>();
+        for(Map.Entry<String, List<String>> day: timetable.entrySet()){
+            Set<String> daySet = new HashSet<>();
+            for(String lesson: day.getValue()){
+                if(daySet.contains(lesson)){
+                    result.remove(lesson);
+                }
+                else {
+                    daySet.add(lesson);
+                result.add(lesson);
+            }
+        }
     }
+        return result;
+}
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,28 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+
+        for(Map.Entry<String, Integer> source: sourceMap.entrySet()){
+            if(result.containsKey(source.getKey().length())){
+                Set<String> currentSet = new HashSet<>(result.get(source.getKey().length()));
+                currentSet.add(source.getKey());
+                result.put(source.getKey().length(), currentSet);
+            }
+            else {
+                Set<String> setter = new HashSet<>();
+                setter.add(source.getKey());
+                result.put(source.getKey().length(), setter);
+            }
+
+        }
+
+        return result;
     }
 }
